c7d1dc64a32d7c934bf62e917ed95e9ac098a839
Morten Dahl <mortendahlcs@gmail.com>
2018-06-13 20:39:44 +0200

    ..

--- src/proof/correct_decrypt.rs
+++ src/proof/correct_decrypt.rs
@@ -2,20 +2,16 @@
 use ::BigInteger as BigInt;
 use ::Paillier as Paillier;
 
-/// Correct opening of ciphertext.
-pub trait CorrectOpening<EK, DK, CT, PT, R> {
-    fn prove(dk: &DK, c: CT) -> (PT, R);
-    fn verify(ek: &EK, c: CT, m: PT, r: R) -> bool;
-}
-
-pub struct CorrectDecryptProof {
+pub struct CorrectOpeningProof {
     m: BigInt,
     r: BigInt,
 }
 
-// pub trait ProveCorrectDecrypt<EK, C> {
-//     fn verify(ek: &EK, c: &C, proof: &CorrectDecryptProof) -> bool;
-// }
+/// Correct opening of ciphertext.
+pub trait CorrectOpening<EK, DK, CT, PT, R> {
+    fn prove(dk: &DK, c: CT) -> CorrectOpeningProof;
+    fn verify(ek: &EK, c: CT, m: PT, r: R) -> bool;
+}
 
 // impl<C> ProveCorrectDecrypt for Paillier {
 //     fn verify(ek: &EK, c: &C, proof: &CorrectDecryptProof) -> bool {
